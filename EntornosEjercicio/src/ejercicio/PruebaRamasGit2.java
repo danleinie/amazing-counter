e99b4fa96f174f3cda707c4ecdfdeb7fe0ce4184
Daniel Leiva Nieto <xleiiva@gmail.com>
2018-12-12 18:26:03 +0100

    Añadido PruebaRamasGit2.java

---
+++ EntornosEjercicio/src/ejercicio/PruebaRamasGit2.java
@@ -0,0 +1,21 @@
+package ejercicio;
+
+public class PruebaRamasGit2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo mira voy a crear un array v2");
+		
+		int [] a = new int[10];
+		
+		for (int i = 0; i < a.length; i++) {
+			a[i]=i+2;
+		}
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+
+	}
+
+}
